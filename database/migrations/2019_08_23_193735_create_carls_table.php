3601a60697aa58d4656fe7764ee3a65c906c3250
Wilber Concepción Lugo <wilber@segurmatica.cu>
2019-08-23 15:50:58 -0400

    Creando los modelos y migraciones Carl y CarlDetail.

---
+++ database/migrations/2019_08_23_193735_create_carls_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarlsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carls', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->date('order_date');
+            $table->date('arrived_date');
+            $table->string('status'); //Active, Pending, Approved, Cancelled, Finished
+
+            //user_id FK
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carls');
+    }
+}
